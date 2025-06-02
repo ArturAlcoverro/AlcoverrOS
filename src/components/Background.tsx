2925fb4bdaa2a2cd6e4418132a93ddd3d6f5d899
artur <artur.alcoverro@dribba.com>
2025-06-02 16:31:40 +0200

    -

--- src/components/Background.tsx
+++
@@ -1,9 +0,0 @@
-import waves_wallpaper from '@assets/images/waves_wallpaper.jpg'
-
-export const Background: React.FC = () => {
-  return (
-    <div className="absolute h-full w-full">
-      <img src={waves_wallpaper} className="h-full w-full object-cover" alt="wallpaper" />
-    </div>
-  )
-}
--- src/components/Texts/index.ts
+++
@@ -1,4 +0,0 @@
-export { H1 } from './h1'
-export { H2 } from './h2'
-export { RegularText } from './regular-text'
-export { SmallText } from './small-text'
